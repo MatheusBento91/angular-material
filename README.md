69a173b39514dfc2921ac36383792dbb3912ceee
Matheus Bento <matheusbentodepereira@gmail.com>
2023-07-10 01:20:43 -0300

    Create project - install Angular material and ngx-translate

---
+++ README.md
@@ -0,0 +1,41 @@
+## Table of contents
+* [General info](#general-info)
+* [Technologies](#technologies)
+* [Setup](#setup)
+
+## General info
+This is a project created with Angular 16.1.6 - Angular material and ngx-translate.
+
+This project suport two languagues English and Portuguese.
+
+Features in this project:
+
+- Lazy loading routes
+- Auth guard
+- Ligth and dark mode 
+- Responsive in perfect pixel
+- Table with pagination and filter
+- Animations with animate.css
+- Implement multiples components of Angular material
+
+## Technologies
+Project is created with:
+* Angular - Typescript - Rxjs
+* Angular material - ngx-translate
+* HTML5 - CSS3 - SASS - Flexbox
+* Cypress - jasmine
+	
+## Setup
+To run this project, install it locally using npm:
+
+```
+$ npm install
+$ ng serve
+```
+
+To run the e2e tests with cypress use:
+
+```
+$ ng serve
+$ ng e2e
+```
---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: (createTranslateLoader),
+          deps: [HttpClient],
+      },
+      defaultLanguage: 'en'
+  })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
